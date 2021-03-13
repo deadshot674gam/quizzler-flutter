a4aa8282a40389bf1ec092c4420b290ae87a2d0c
deadshot674gam <sunnyt0225@gmail.com>
2021-03-14 00:20:35 +0530

    changes made in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Question> questionList = [
+    new Question('You can lead a cow down stairs but not up stairs.', false),
+    new Question(
+        'Approximately one quarter of human bones are in the feet.', true),
+    new Question('A slug\'s blood is green.', true)
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -32,21 +42,20 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  questionList[questionNumber].question,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ),
-        ),
+            )),
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -61,7 +70,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                if (questionList[questionNumber].answer == true) {}
+
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
@@ -80,11 +93,20 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                if (questionList[questionNumber].answer == false) {}
+
+                
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: <Widget>[],
+        )
       ],
     );
   }
